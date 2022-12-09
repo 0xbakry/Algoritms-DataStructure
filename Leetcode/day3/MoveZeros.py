1bad8b5346273b7ce23f5d8b36465d4d0f405512
Omar Bakry <67487202+0xbakry@users.noreply.github.com>
2022-12-09 14:10:37 +0200

    Create MoveZeros.py

---
+++ Leetcode/day3/MoveZeros.py
@@ -0,0 +1,18 @@
+"""
+The problem: 
+Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+Note that you must do this in-place without making a copy of the array.
+"""
+
+"""
+Since we can control how to sort the array, based on our criteria, we can use it as following 
+"""
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        nums.sort(key=lambda target:target==0)
+        
